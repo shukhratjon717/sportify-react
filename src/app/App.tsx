cb96514b4d27bbf076eccc28729025a730874ac7
Shukhratjon <shukhratjon0717@gmail.com>
2024-05-10 07:38:37 +0900

    feat: develop Sportify screen based components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,44 +2,53 @@ import React from "react";
 import "../css/app.css";
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import { Link, Route, Switch } from "react-router-dom";
-import { About } from "./screns/About";
-import { Users } from "./screns/Users";
+import { OrdersPage } from "./screns/ordersPage";
+import { ProductsPage } from "./screns/productsPage";
+import { UserPage } from "./screns/userPage";
+import { HomePage } from "./screns/homePage";
 
 function App() {
   return (
-   <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-        
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">HomePage</Link>
+          </li>
+          <li>
+            <Link to="/products">ProductsPage</Link>
+          </li>
+          <li>
+            <Link to="/orders">OrdersPage</Link>
+          </li>
+          <li>
+            <Link to="/member-page">UserPage</Link>
+          </li>
+          <li>
+            <Link to="/">HomePage</Link>
+          </li>
+        </ul>
+      </nav>
 
+      <Switch>
+        <Route path="/products">
+          <ProductsPage />
+        </Route>
+        <Route path="/orders">
+          <OrdersPage />
+        </Route>
+        <Route path="/member-page">
+          <UserPage />
+        </Route>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+    </div>
   );
 }
 function Home() {
   return <Container>Home</Container>;
 }
-
 
 export default App;
