bd6dc17658c0a4361fb98c4e9ecc633346325a1f
Shukhratjon <shukhratjon0717@gmail.com>
2024-05-16 13:43:19 +0900

    feat: develop productPage nested routing system

--- src/app/screns/productsPage/index.tsx
+++ src/app/screns/productsPage/index.tsx
@@ -1,6 +1,21 @@
+import React from "react";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
 import { Container } from "@mui/material";
+import Products from "./Products";
+import ChosenProduct from "./ChosenProduct";
 
 export default function ProductsPage() {
-    return <Container>ProductsPage</Container>;
-  }
-  
+  const products = useRouteMatch();
+  console.log("products:", products);
+  return (
+    <div className={"products-page"}>
+      <Switch>
+        <Route path={`${products.path}/:productId`}>
+          <ChosenProduct />
+        </Route>
+        <Route path={`${products.path}/:productId`}></Route>
+        <Products />
+      </Switch>
+    </div>
+  );
+}
